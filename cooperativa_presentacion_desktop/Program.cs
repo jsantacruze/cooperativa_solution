48d67f92cc54e1022405a739d318f1cb9b68c882
JavierEspinoza2014 <javierespi082691@gmail.com>
2022-05-30 18:02:15 -0500

    Javier

--- cooperativa_presentacion_desktop/Program.cs
+++ cooperativa_presentacion_desktop/Program.cs
@@ -15,7 +15,7 @@ namespace cooperativa_presentacion_desktop
         static void Main()
         {
             Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
+            Application.SetCompatibleTextRenderingDefault(false); 
             Application.Run(new frmPrincipal());
         }
     }
